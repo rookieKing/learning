cb6302759ce182a65582ca0539955de4b950c5f5
rookie <rookielei@gmail.com>
2022-07-29 10:44:30 +0800

    WebGL 三维正射投影 三维中的 F

--- WebGL/src/glsl/vertex-shader-2d.glsl
+++ WebGL/src/glsl/vertex-shader-2d.glsl
@@ -1,10 +1,10 @@
 // 一个属性变量，将会从缓冲中获取数据
-attribute vec2 a_position;
+attribute vec4 a_position;
 
-uniform mat3 u_matrix;
+uniform mat4 u_matrix;
 
 // 所有着色器都有一个main方法
 void main() {
   // 使位置和矩阵相乘
-  gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
+  gl_Position = u_matrix * a_position;
 }
--- WebGL/src/index.js
+++ WebGL/src/index.js
@@ -40,134 +40,166 @@ function setGeometry(gl, x = 0, y = 0) {
     gl.ARRAY_BUFFER,
     new Float32Array([
       // 左竖
-      x, y,
-      x + thickness, y,
-      x, y + height,
-      x, y + height,
-      x + thickness, y,
-      x + thickness, y + height,
+      x, y, 0,
+      x + thickness, y, 0,
+      x, y + height, 0,
+      x, y + height, 0,
+      x + thickness, y, 0,
+      x + thickness, y + height, 0,
 
       // 上横
-      x + thickness, y,
-      x + width, y,
-      x + thickness, y + thickness,
-      x + thickness, y + thickness,
-      x + width, y,
-      x + width, y + thickness,
+      x + thickness, y, 0,
+      x + width, y, 0,
+      x + thickness, y + thickness, 0,
+      x + thickness, y + thickness, 0,
+      x + width, y, 0,
+      x + width, y + thickness, 0,
 
       // 中横
-      x + thickness, y + thickness * 2,
-      x + width * 2 / 3, y + thickness * 2,
-      x + thickness, y + thickness * 3,
-      x + thickness, y + thickness * 3,
-      x + width * 2 / 3, y + thickness * 2,
-      x + width * 2 / 3, y + thickness * 3,
+      x + thickness, y + thickness * 2, 0,
+      x + width * 2 / 3, y + thickness * 2, 0,
+      x + thickness, y + thickness * 3, 0,
+      x + thickness, y + thickness * 3, 0,
+      x + width * 2 / 3, y + thickness * 2, 0,
+      x + width * 2 / 3, y + thickness * 3, 0,
     ]),
     gl.STATIC_DRAW);
 }
 
-var m3 = {
-  identity: function () {
+var m4 = {
+  projection: function (width, height, depth) {
     return [
-      1, 0, 0,
-      0, 1, 0,
-      0, 0, 1,
+      2 / width, 0, 0, 0,
+      0, -2 / height, 0, 0,
+      0, 0, 2 / depth, 0,
+      -1, 1, 0, 1,
     ];
   },
 
-  projection: function (width, height) {
-    /*
-    vec2 zeroToOne = position / u_resolution;
-    vec2 zeroToTwo = zeroToOne * 2.0;
-    vec2 clipSpace = zeroToTwo - 1.0;
-    clipSpace.y *= -1.0; // clipSpace *= vec2(1, -1);
-    */
-
-    // var zeroToOne = m3.scaling(1 / width, 1 / height);
-    // var zeroToTwo = m3.scaling(2, 2);
-    // var clipSpace = m3.translation(-1, -1);
-    // var scaleY = m3.scaling(1, -1);
-    // var projectionMatrix; // 顺序很重要
-    // projectionMatrix = m3.multiply(scaleY, clipSpace);
-    // projectionMatrix = m3.multiply(projectionMatrix, zeroToTwo);
-    // projectionMatrix = m3.multiply(projectionMatrix, zeroToOne);
-    // return projectionMatrix;
-
-    // 可以从上面的计算直接得出这个结果
+  translation: function (tx, ty, tz) {
     return [
-      2 / width, 0, 0,
-      0, -2 / height, 0,
-      -1, 1, 1
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      tx, ty, tz, 1,
     ];
   },
 
-  translation: function (tx, ty) {
-    return [
-      1, 0, 0,
-      0, 1, 0,
-      tx, ty, 1,
-    ];
-  },
-
-  rotation: function (angleInRadians) {
+  xRotation: function (angleInRadians) {
     var c = Math.cos(angleInRadians);
     var s = Math.sin(angleInRadians);
+
     return [
-      c, -s, 0,
-      s, c, 0,
-      0, 0, 1,
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0, -s, c, 0,
+      0, 0, 0, 1,
     ];
   },
 
-  scaling: function (sx, sy) {
+  yRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
     return [
-      sx, 0, 0,
-      0, sy, 0,
-      0, 0, 1,
+      c, 0, -s, 0,
+      0, 1, 0, 0,
+      s, 0, c, 0,
+      0, 0, 0, 1,
     ];
   },
 
-  translate: function (m, tx, ty) {
-    return m3.multiply(m, m3.translation(tx, ty));
+  zRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      c, s, 0, 0,
+      -s, c, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1,
+    ];
   },
 
-  rotate: function (m, angleInRadians) {
-    return m3.multiply(m, m3.rotation(angleInRadians));
+  scaling: function (sx, sy, sz) {
+    return [
+      sx, 0, 0, 0,
+      0, sy, 0, 0,
+      0, 0, sz, 0,
+      0, 0, 0, 1,
+    ];
   },
 
-  scale: function (m, sx, sy) {
-    return m3.multiply(m, m3.scaling(sx, sy));
+  translate: function (m, tx, ty, tz) {
+    return m4.multiply(m, m4.translation(tx, ty, tz));
+  },
+
+  xRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.xRotation(angleInRadians));
+  },
+
+  yRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.yRotation(angleInRadians));
+  },
+
+  zRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.zRotation(angleInRadians));
+  },
+
+  scale: function (m, sx, sy, sz) {
+    return m4.multiply(m, m4.scaling(sx, sy, sz));
   },
 
   multiply: function (a, b) {
-    var a00 = a[0 * 3 + 0];
-    var a01 = a[0 * 3 + 1];
-    var a02 = a[0 * 3 + 2];
-    var a10 = a[1 * 3 + 0];
-    var a11 = a[1 * 3 + 1];
-    var a12 = a[1 * 3 + 2];
-    var a20 = a[2 * 3 + 0];
-    var a21 = a[2 * 3 + 1];
-    var a22 = a[2 * 3 + 2];
-    var b00 = b[0 * 3 + 0];
-    var b01 = b[0 * 3 + 1];
-    var b02 = b[0 * 3 + 2];
-    var b10 = b[1 * 3 + 0];
-    var b11 = b[1 * 3 + 1];
-    var b12 = b[1 * 3 + 2];
-    var b20 = b[2 * 3 + 0];
-    var b21 = b[2 * 3 + 1];
-    var b22 = b[2 * 3 + 2];
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
     return [
-      b00 * a00 + b01 * a10 + b02 * a20,
-      b00 * a01 + b01 * a11 + b02 * a21,
-      b00 * a02 + b01 * a12 + b02 * a22,
-      b10 * a00 + b11 * a10 + b12 * a20,
-      b10 * a01 + b11 * a11 + b12 * a21,
-      b10 * a02 + b11 * a12 + b12 * a22,
-      b20 * a00 + b21 * a10 + b22 * a20,
-      b20 * a01 + b21 * a11 + b22 * a21,
-      b20 * a02 + b21 * a12 + b22 * a22,
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
     ];
   },
 };
@@ -183,11 +215,14 @@ var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 var colorUniformLocation = gl.getUniformLocation(program, "u_color");
 var positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+// 将几何数据存到缓冲
+setGeometry(gl);
 
-var scale = [.85, .85];
-var translation = [60, 40];
-var angleInRadians = 0;
+var translation = [45, 150, 0];
+var rotation = [degToRad(40), degToRad(25), degToRad(325)];
+var scale = [1, 1, 1];
 var color = [Math.random(), Math.random(), Math.random(), 1];
+
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -199,9 +234,13 @@ resize();
 // Setup a ui.
 webglLessonsUI.setupSlider("#x", { value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
 webglLessonsUI.setupSlider("#y", { value: translation[1], slide: updatePosition(1), max: gl.canvas.height });
-webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
+webglLessonsUI.setupSlider("#z", { value: translation[2], slide: updatePosition(2), max: gl.canvas.height });
+webglLessonsUI.setupSlider("#angleX", { value: radToDeg(rotation[0]), slide: updateRotation(0), max: 360 });
+webglLessonsUI.setupSlider("#angleY", { value: radToDeg(rotation[1]), slide: updateRotation(1), max: 360 });
+webglLessonsUI.setupSlider("#angleZ", { value: radToDeg(rotation[2]), slide: updateRotation(2), max: 360 });
 webglLessonsUI.setupSlider("#scaleX", { value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2 });
 webglLessonsUI.setupSlider("#scaleY", { value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2 });
+webglLessonsUI.setupSlider("#scaleZ", { value: scale[2], slide: updateScale(2), min: -5, max: 5, step: 0.01, precision: 2 });
 
 function updateScale(index) {
   return function (event, ui) {
@@ -210,10 +249,13 @@ function updateScale(index) {
   };
 }
 
-function updateAngle(event, ui) {
-  var angleInDegrees = 360 - ui.value;
-  angleInRadians = angleInDegrees * Math.PI / 180;
-  drawScene();
+function updateRotation(index) {
+  return function (event, ui) {
+    var angleInDegrees = ui.value;
+    var angleInRadians = angleInDegrees * Math.PI / 180;
+    rotation[index] = angleInRadians;
+    drawScene();
+  };
 }
 
 function updatePosition(index) {
@@ -221,6 +263,14 @@ function updatePosition(index) {
     translation[index] = ui.value;
     drawScene();
   };
+}
+
+function radToDeg(r) {
+  return r * 180 / Math.PI;
+}
+
+function degToRad(d) {
+  return d * Math.PI / 180;
 }
 
 function drawScene() {
@@ -236,7 +286,7 @@ function drawScene() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
-  var size = 2;          // 每次迭代运行提取两个单位数据
+  var size = 3;          // 每次迭代运行提取两个单位数据
   var type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
   var normalize = false; // 不需要归一化数据
   var stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
@@ -245,25 +295,18 @@ function drawScene() {
   gl.vertexAttribPointer(
     positionAttributeLocation, size, type, normalize, stride, offset)
 
-  // 将几何数据存到缓冲
-  setGeometry(gl);
   // 设置颜色
   gl.uniform4fv(colorUniformLocation, color);
   // 计算矩阵
-  var matrix = m3.projection(
-    gl.canvas.clientWidth, gl.canvas.clientHeight);
-  // 初始矩阵
-  // var matrix = m3.identity();
-  for (var i = 0; i < 5; ++i) {
-    // 矩阵相乘
-    matrix = m3.translate(matrix, translation[0], translation[1]);
-    matrix = m3.rotate(matrix, angleInRadians);
-    matrix = m3.scale(matrix, scale[0], scale[1]);
+  var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+  matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+  matrix = m4.xRotate(matrix, rotation[0]);
+  matrix = m4.yRotate(matrix, rotation[1]);
+  matrix = m4.zRotate(matrix, rotation[2]);
+  matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+  // 设置矩阵
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
-    // 设置矩阵
-    gl.uniformMatrix3fv(matrixLocation, false, matrix);
-
-    // 绘制矩形
-    gl.drawArrays(gl.TRIANGLES, 0, 18);
-  }
+  // 绘制矩形
+  gl.drawArrays(gl.TRIANGLES, 0, 18);
 }
